3b8f826c37ab39df8cc44632d83ab63833c31b18
Logan Blackstad <logan.blackstad@gmail.com>
2020-09-03 11:41:11 -0400

    simple addition funtion

---
+++ javascript/js-simple-addition-function.js
@@ -0,0 +1,16 @@
+
+// Description:
+// a simply JavaScript addition function
+//
+// author(s): Logan Blackstad
+// date:      Sep 03 2020
+// 
+
+// to run in the terminal, simply type "node fileName.js"
+// 
+
+function add(a, b) {
+  return a + b
+}
+console.log(add(4, 6))
+
