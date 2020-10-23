56053bb3bd4e8c65776fadc7c7be62c0ba90b52c
Logan Blackstad <logan.blackstad@gmail.com>
2020-10-23 09:56:54 -0400

    finally able to parse a string of space seperated numbers into an array of integers

--- javascript/challenge-bowling/score.js
+++ javascript/challenge-bowling/score.js
@@ -9,11 +9,16 @@ const readline = require('readline');
 var text = fs.readFileSync("./bowl1.txt", "utf-8");
 var textByLine = text.split("\n");
 
-var n = textByLine[0]
-var rolls = textByLine[1]
+var n = parseInt(textByLine[0], 10);
+var rolls = textByLine[1].split(" ");
+var rollsInNumber = rolls.map(Number);
+
+// var rolls = textByLine[1].map(Number);
+
 
 console.log(n);
 console.log(rolls);
+console.log(rollsInNumber);
 
 
 // loop through the array of rolls and generate an array of frames
@@ -21,12 +26,38 @@ console.log(rolls);
 var frames = 0;
 var frameArr = [];
 
-for (i = 0; i < n, i++) {
+for (i = 0; i < n; i++) {
+  // console.log(`\n`);
+  // console.log(`2 balls: `, rolls[i], rolls[i + 1]);
+  // console.log('1: ', rolls[i]);
+  // console.log('2: ', rolls[i + 1]);
 
+  console.log(`frames: `, frames);
+  console.log(`frameArr: `, frameArr);
+
+  // strike
+  if (rolls[i] === 10) {
+    console.log('strike')
+    frameArr.push(10);
+    frames += 1;
+  }
+
+  // spare
+  else if ((rolls[i] + rolls[i + 1]) === 10) {
+    console.log('spare')
+    frameArr.push(10);
+    frames += 1;
+    i++;
+  }
+
+  // base
+  else {
+    console.log('base')
+    frameArr.push((rolls[i] + rolls[i + 1]));
+    frames += 1;
+    i++;
+  }
 }
 
-
-
-//
-
-* /
+console.log(frames);
+console.log(frameArr);
