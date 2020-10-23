d7e5ad1ec557ed09036e92f7c18db80f0451c579
Logan Blackstad <logan.blackstad@gmail.com>
2020-10-23 11:24:18 -0400

    still working out the logic

--- javascript/challenge-bowling/score.js
+++ javascript/challenge-bowling/score.js
@@ -6,7 +6,7 @@
 
 var fs = require("fs");
 const readline = require('readline');
-var text = fs.readFileSync("./bowl1.txt", "utf-8");
+var text = fs.readFileSync("./bowl2.txt", "utf-8");
 var textByLine = text.split("\n");
 
 var n = parseInt(textByLine[0], 10);
@@ -14,7 +14,6 @@ var rolls = textByLine[1].split(" ");
 var rollsInNumber = rolls.map(Number);
 
 // var rolls = textByLine[1].map(Number);
-
 
 console.log(n);
 console.log(rolls);
@@ -25,39 +24,55 @@ console.log(rollsInNumber);
 
 var frames = 0;
 var frameArr = [];
+rolls = rollsInNumber;
+var scoreArr = [];
+var scoreArr2 = [0,];
 
 for (i = 0; i < n; i++) {
-  // console.log(`\n`);
-  // console.log(`2 balls: `, rolls[i], rolls[i + 1]);
-  // console.log('1: ', rolls[i]);
-  // console.log('2: ', rolls[i + 1]);
-
+  console.log(`\n`);
+  console.log(`balls: `, rolls[i], rolls[i + 1]);
   console.log(`frames: `, frames);
   console.log(`frameArr: `, frameArr);
+  console.log(`scoreArr: `, scoreArr);
+  console.log(`scoreArr2: `, scoreArr2);
+  console.log(`balls left:`, n - i)
 
-  // strike
-  if (rolls[i] === 10) {
-    console.log('strike')
-    frameArr.push(10);
-    frames += 1;
-  }
+  if (n - i > 1) {
+    // strike
+    if (rolls[i] === 10 && (n - i > 2 && ((isNaN(rolls[i + 2]) ? 0 : rolls[i + 2]) !== 10))) {
+      console.log('strike')
+      frameArr.push(10);
+      scoreArr.push(10 + rolls[i + 1] + (isNaN(rolls[i + 2]) ? 0 : rolls[i + 2]));
+      scoreArr2.push(scoreArr2[scoreArr2.length - 1] + 10 + rolls[i + 1] + (isNaN(rolls[i + 2]) ? 0 : rolls[i + 2]));
+      frames += 1;
+    }
 
-  // spare
-  else if ((rolls[i] + rolls[i + 1]) === 10) {
-    console.log('spare')
-    frameArr.push(10);
-    frames += 1;
-    i++;
-  }
+    // spare
+    else if ((rolls[i] + rolls[i + 1]) === 10) {
+      console.log('spare')
+      frameArr.push(10);
+      scoreArr.push(10 + (isNaN(rolls[i + 2]) ? 0 : rolls[i + 2]));
+      scoreArr2.push(scoreArr2[scoreArr2.length - 1] + 10 + (isNaN(rolls[i + 2]) ? 0 : rolls[i + 2]));
+      frames += 1;
+      i++;
+    }
 
-  // base
-  else {
-    console.log('base')
-    frameArr.push((rolls[i] + rolls[i + 1]));
-    frames += 1;
-    i++;
+    // base
+    else {
+      console.log('base')
+      frameArr.push((rolls[i] + rolls[i + 1]));
+      scoreArr.push((rolls[i] + rolls[i + 1]))
+      scoreArr2.push(scoreArr2[scoreArr2.length - 1] + (rolls[i] + rolls[i + 1]));
+      frames += 1;
+      i++;
+    }
   }
 }
 
+var final = scoreArr2[scoreArr2.length - 1]
+
 console.log(frames);
 console.log(frameArr);
+console.log(scoreArr);
+console.log(scoreArr2);
+console.log(`\nfinal:\n`, final)
