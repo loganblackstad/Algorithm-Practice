be78cdca86a79aae2c5914fb586a0c0a09857b90
Blackstad <lblackst@na.ops.local>
2021-06-08 08:03:46 -0400

    javascript practice

---
+++ javascript/js-remove-vowels.js
@@ -0,0 +1,19 @@
+
+
+
+// Description:
+// write a JavaScript function that removes all the vowels from the word
+//
+// author(s): Logan Blackstad
+// date:      Jun 08 2021
+// 
+
+// to run in the terminal, simply type "node fileName.js"
+// 
+
+function removeVowels(str1) {
+    return str1[0];
+}
+  
+
+console.log(removeVowels('test'))
